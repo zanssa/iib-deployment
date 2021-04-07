ef09040e0ff172436e2b959952b75e10d9eb7d24
Zainab Alsaffar <zalsaffa@localhost.localdomain>
2021-04-07 16:36:45 -0400

    modified dockerfile

--- tower.sh
+++ tower.sh
@@ -5,8 +5,8 @@ if [ $# -lt 4 ]
 then
     echo "One or more of the required arguments is not passed"
 else
-    awx --conf.host "$TOWER_HOST" \
-        --conf.token "$TOWER_OAUTH" \
-        --conf.username "$TOWER_USER" \
-        -k job_templates launch "$TOWER_JOB" --monitor -f human
+    awx --conf.host "$tower_host" \
+        --conf.token "$tower_oauth" \
+        --conf.username "$tower_user" \
+        -k job_templates launch "$tower_job" --monitor -f human
 fi
