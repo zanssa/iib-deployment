8fd8dfcc2ada00871c48d6cfd91128390200e5c8
Zainab Alsaffar <zalsaffa@localhost.localdomain>
2021-04-23 14:05:47 -0400

    testing for oc

--- tower.sh
+++ tower.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-echo "Verify all arguments have been passed"
+echo "Verify all awx cmd arguments have been passed"
 if [ $# -lt 4 ]
 then
     echo "One or more of the required arguments is not passed"
 else
-    echo "host" $1
-    echo "'$4'"
+    echo "Tower: " $1
+    echo "Running Job, '$4'"
     awx --conf.host $1 \
         --conf.token $2 \
         --conf.username $3 \
         -k workflow_job_templates launch "$4" -f human
 fi
+
+while true; do echo 'test'; sleep 2; done
