f5c4dff8bf0980d231a471662829f89c0a4b9048
Zainab Alsaffar <zalsaffa@localhost.localdomain>
2021-04-23 15:59:25 -0400

    test labels

--- test.py
+++ test.py
@@ -1,8 +1,6 @@
 # this is for testing github workflow actions
 # this is for testing github workflow actions
 # this is for testing github workflow actions
-# this is for testing github workflow actions
-# this is for testing github workflow actions
 
 import sys
 import sys
